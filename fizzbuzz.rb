58e7c7a413f673b1bad390f676613143957bea5a
Jennifer Vargas <jevargasv1990@gmail.com>
2019-09-25 19:25:19 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 def fizzbuzz(num)
   if(num % 5 == 0 && num % 3 == 0)
-    puts "FizzBuzz"
+    "FizzBuzz"
   elsif(num % 3 == 0)
-    puts "Fizz"
+    "Fizz"
   elsif(num % 5 == 0)
-    puts "Buzz"
+    "Buzz"
   else
-    puts nil
+    nil
   end
 end
